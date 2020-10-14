680a689bdbfc183d3204031104a982d2efcafc14
collins koech <collinskoech875@gmail.com>
2020-10-14 18:30:40 +0300

    define variables

--- password_lock.py
+++ password_lock.py
@@ -12,3 +12,8 @@ class User:
         '''
 
         User.user_list.append(self)
+        def __init__(self,first_name,last_name,password):
+  
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
