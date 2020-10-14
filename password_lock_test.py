cb238901215fe7d0b33266b20866cfe14c5971dc
collins koech <collinskoech875@gmail.com>
2020-10-14 18:29:44 +0300

    add class testuser to file

--- password_lock_test.py
+++ password_lock_test.py
@@ -1,2 +1,13 @@
 import unittest  # Importing the unittest module
 from Password_Lock import User # Importing the contact class
+
+
+class TestUser(unittest.TestCase):
+  
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    # Items up here .......
