0925b5e645f967c35e4722d82c7595d799bce63e
collins koech <collinskoech875@gmail.com>
2020-10-14 18:52:58 +0300

    add method to save contact objects

--- password_lock.py
+++ password_lock.py
@@ -5,3 +5,10 @@ class User:
     class that generates new instances of Users
     """
     user_list = []
+def save_user(self):
+  
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        User.user_list.append(self)
