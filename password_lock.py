ff0a45b5226ee83078f3b4ad2b3bea3b2338e369
collins koech <collinskoech875@gmail.com>
2020-10-14 18:54:49 +0300

    add classmethod to find by password

--- password_lock.py
+++ password_lock.py
@@ -25,3 +25,17 @@ def __init__(self,first_name,last_name,password):
         delete_contact method
         '''
         User.user_list.remove(self)
+ @classmethod
+    def find_by_password(cls,password):
+        '''
+        Method that takes in a password and returns a contact that matches that password.
+
+        Args:
+            password: password to search for
+            Returns :
+            Credentials of person that matches the password.
+            '''
+
+        for user in cls.user_list:
+            if user.password == password:
+                return user
