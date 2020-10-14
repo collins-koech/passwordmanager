4ff9cd0fc1442ae3ffb7d49c5faeec476ddf0093
collins koech <collinskoech875@gmail.com>
2020-10-14 18:39:08 +0300

    add first test

--- password_lock.py
+++ password_lock.py
@@ -21,3 +21,22 @@ def setUp(self):
         Set up method to run before each test cases.
         '''
         self.new_user = User("James","Muriuki","asdfgh890") # create contact object
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("James","Muriuki","asdfgh890") # create contact object
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"James")
+        self.assertEqual(self.new_user.last_name,"Muriuki")
+        self.assertEqual(self.new_user.password,"asdfgh890")
