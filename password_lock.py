567168a3482134d83a87b47f1d6c0fb61bec3f8c
collins koech <collinskoech875@gmail.com>
2020-10-14 18:42:52 +0300

    add test to know if user exists

--- password_lock.py
+++ password_lock.py
@@ -84,3 +84,15 @@ def test_find_user_by_password(self):
         found_user = User.find_by_password("asdfgh8901")
 
         self.assertEqual(found_user.first_name,test_user.first_name)
+def test_user_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the contact.
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Test","user","asdfgh8901",) # new contact
+        test_user.save_user()
+
+        user_exists = User.user_exist("asdfgh8901")
+
+        self.assertTrue(user_exists)
