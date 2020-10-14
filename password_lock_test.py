61c3ac32537b833fcdcd25614b426addd59dc718
collins koech <collinskoech875@gmail.com>
2020-10-14 18:44:18 +0300

    create contact object

--- password_lock_test.py
+++ password_lock_test.py
@@ -11,3 +11,9 @@ class TestUser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
     # Items up here .......
+
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("James","Muriuki","asdfgh890") # create contact object
