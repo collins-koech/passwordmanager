cbb417780719123064292302e8e7b021db76312f
collins koech <collinskoech875@gmail.com>
2020-10-14 18:53:53 +0300

    add delete contact method

--- password_lock.py
+++ password_lock.py
@@ -12,3 +12,16 @@ def save_user(self):
         '''
 
         User.user_list.append(self)
+
+def __init__(self,first_name,last_name,password):
+  
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def delete_user(self):
+
+        '''
+        delete_contact method
+        '''
+        User.user_list.remove(self)
